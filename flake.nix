31503b6e9f9010ddb37a5c2847c24cbc81f501dd
zyansheep <zyansheep@github.com>
2021-10-12 07:59:14 -0400

    update flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,35 @@
 {
-  description = "Flutter Flake";
+	description = "Flutter Flake";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+		utils.url = "github:numtide/flake-utils";
+	};
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.simpleFlake {
-		inherit self nixpkgs;
-		name = "flutter-project";
-		overlay = null;
-		shell = ./shell.nix;
-    };
+	outputs = { self, nixpkgs, utils }:
+	utils.lib.eachDefaultSystem (system: let
+		pkgs = import nixpkgs {
+			inherit system;
+			config = { allowUnfree = true; };
+		};
+	in rec {
+		# `nix develop`
+		devShell = pkgs.mkShell {
+			buildInputs = with pkgs; [
+				flutter
+				dart
+				android-studio
+				jdk
+				git
+				chromium
+			];
+			shellHook=''
+				export USE_CCACHE=1
+				export ANDROID_JAVA_HOME=${pkgs.jdk.home}
+				export ANDROID_HOME=~/.androidsdk
+				export FLUTTER_SDK=${pkgs.flutter}
+				export CHROME_EXECUTABLE="chromium"
+			'';
+		};
+	});
 }
