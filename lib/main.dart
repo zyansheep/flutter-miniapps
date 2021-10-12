2bea8c32a5bfdab78d5890b7a7bb9253bf88f049
zyansheep <zyansheep@github.com>
2021-10-12 08:48:06 -0400

    colored boxes : )

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,7 +31,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key? key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -39,7 +42,7 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+  final String? title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -68,46 +71,73 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
+      /* appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+        title: Text(widget.title!),
+      ), */
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(0),
+        crossAxisSpacing: 0,
+        mainAxisSpacing: 0,
+        crossAxisCount: 3,
+        children: <Widget>[
+          RandomColorBox(milliseconds: 1000),
+          RandomColorBox(milliseconds: 2000),
+          RandomColorBox(milliseconds: 500),
+          RandomColorBox(milliseconds: 500),
+          RandomColorBox(),
+          RandomColorBox(milliseconds: 2000),
+          RandomColorBox(),
+          RandomColorBox(milliseconds: 100),
+          RandomColorBox(milliseconds: 400),
+        ],
+      )),
+      /* floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),  */ // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class RandomColorBox extends StatefulWidget {
+  const RandomColorBox({
+    Key? key,
+    this.milliseconds = 1000,
+  }) : super(key: key);
+
+  final int milliseconds;
+
+  @override
+  _RandomColorBoxState createState() => _RandomColorBoxState();
+}
+
+class _RandomColorBoxState extends State<RandomColorBox> {
+  var color = Colors.black;
+
+  void updateColor() {
+    setState(() {
+      color = Colors.primaries[Random().nextInt(Colors.primaries.length)];
+    });
+  }
+
+  @override
+  void initState() {
+    Timer.periodic(Duration(milliseconds: widget.milliseconds),
+        (Timer t) => updateColor());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
     );
   }
 }
