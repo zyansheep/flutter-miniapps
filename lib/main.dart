eae58e5d74cd0b54628db6a2663b31971d514071
zyansheep <zyansheep@github.com>
2021-11-04 17:31:09 -0400

    fix flutter analyze issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+// import 'package:flutter/rendering.dart';
 
 import 'mobile_disco/mobile_disco.dart';
 import 'add_things/add_things.dart';
