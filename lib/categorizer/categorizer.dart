e03f37bd3b3e268173d9314a6bbbbf5c7a9cc81c
zyansheep <zyansheep@github.com>
2022-01-17 19:48:18 -0500

    fix categorizer, start on clicker game

--- lib/categorizer/categorizer.dart
+++ lib/categorizer/categorizer.dart
@@ -1,25 +1,71 @@
-import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
+import 'dart:io';
 
-class Categorizer extends StatelessWidget {
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+//import 'package:cached_network_image/cached_network_image.dart';
+
+class Categorizer extends StatefulWidget {
+  @override
+  State<Categorizer> createState() => _CategorizerState();
+}
+
+class _CategorizerState extends State<Categorizer> {
+  final List<AssetImage> images = [
+    "20190306_091010.jpg",
+    "20190306_091045.jpg",
+    "20190306_091345.jpg",
+    "20190312_131718.jpg",
+    "20190312_131833.jpg",
+    "20190312_132107.jpg",
+    "20190312_132223.jpg",
+    "20190312_132430.jpg",
+  ].map((e) => AssetImage("kramdr_photos/" + e)).toList();
+  List<List<bool>> isSelected = List.filled(8, [false, true, false]);
+
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: new AppBar(title: Text("Categorizer 3000")),
-      body: Center(
-        child: Container(
-          child: CachedNetworkImage(
-            placeholder: (context, url) =>
-                Center(child: CircularProgressIndicator()),
-            imageUrl: 'https://picsum.photos/250?image=9',
-          ),
-        ),
-      ),
-    );
+        appBar: new AppBar(title: Text("Categorizer 3000")),
+        body: Container(
+            child: Center(
+                child: CarouselSlider.builder(
+                options: CarouselOptions(
+            height: 500,
+                  aspectRatio: 16 / 9,
+                  viewportFraction: 0.9,
+                  initialPage: 0,
+                  enableInfiniteScroll: false,
+                  autoPlay: false,
+                  scrollDirection: Axis.horizontal,
+                ),
+          itemCount: images.length,
+                itemBuilder:
+                    (BuildContext context, int itemIndex, int pageViewIndex) =>
+                        Column(children: [
+            Image(image: images[itemIndex]),
+                          ToggleButtons(
+                            children: <Widget>[
+                              Icon(Icons.check_circle_outlined),
+                              Icon(Icons.adjust_outlined),
+                              Icon(Icons.block),
+                            ],
+                            onPressed: (int index) {
+                              setState(() {
+                                for (int buttonIndex = 0;
+                                    buttonIndex < isSelected[itemIndex].length;
+                                    buttonIndex++) {
+                                  if (buttonIndex == index) {
+                                    isSelected[itemIndex][buttonIndex] =
+                                        !isSelected[itemIndex][buttonIndex];
+                                  } else {
+                                    isSelected[itemIndex][buttonIndex] = false;
+                                  }
+                                }
+                              });
+                            },
+                            isSelected: isSelected[itemIndex],
+                          ),
+          ]),
+        ))));
   }
 }
